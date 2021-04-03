4d1164aa2dcdf7982fc2a758840a03cd663235c2
Sparrow48 <abunasib48@gmail.com>
2021-04-03 10:46:30 +0600

    Start implementing BurgerBuilder component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import classes from "./App.module.css";
 
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
-        <Layout></Layout>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
