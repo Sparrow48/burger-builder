16b1428b2f3134f82b477c61f7feca83532c0a12
Sparrow48 <abunasib48@gmail.com>
2021-04-11 12:57:18 +0600

    Add sidedrawer for mobile device

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,11 @@ import React from "react";
 import Aux from "../../hoc/aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Aux>
+    <SideDrawer />
     <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
