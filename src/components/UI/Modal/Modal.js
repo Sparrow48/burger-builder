12fa0b635554843742778e90b173a4252c67a283
Sparrow48 <abunasib48@gmail.com>
2021-04-12 11:13:41 +0600

    Improve performance with class based components

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,33 +1,36 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
 import Aux from "../../../hoc/aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => {
-  useEffect(() => {
-    console.log("Modal");
-  });
-
-  return (
-    <Aux>
-      <Backdrop show={props.show} cancelBackDrop={props.cancelPurchase} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}>
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
-
-export default React.memo(Modal, (preProps, nextProps) => {
-  if (preProps.show !== nextProps.show) {
-    return false;
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
   }
 
-  return true;
-});
+  componentDidUpdate() {
+    console.log("modal");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          cancelBackDrop={this.props.cancelPurchase}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
