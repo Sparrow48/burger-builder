cda74d8b6a10230ec121a2b08feb1d3e078571b1
Sparrow48 <abunasib48@gmail.com>
2021-04-16 22:13:55 +0600

    Fatching order data from firebase database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
 
 class Orders extends Component {
+  state = {
+    order: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then(res => {
+        const fetchedData = [];
+        for (let key in res.data) {
+          fetchedData.push({ ...res.data[key], id: key });
+        }
+        this.setSate({ loading: false, Orders: fetchedData });
+      })
+      .catch(err => {
+        this.setSate({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
@@ -13,4 +34,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
