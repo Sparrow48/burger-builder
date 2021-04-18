7ac30aa829f7b8ef56ddca32ace6113f92bc680e
Sparrow48 <abunasib48@gmail.com>
2021-04-18 20:39:41 +0600

    Finishing the Reducer for Ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,38 @@
 import * as actionType from "./actions";
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    cheese: 0,
+    bacon: 0,
+    meat: 0,
+    egg: 0,
+    vegetable: 0,
+  },
   totalPrice: 25,
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+      };
+    case actionType.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
